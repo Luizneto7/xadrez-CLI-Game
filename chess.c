6f8850213fdaf5ff44171a134612d4a3efbb3de0
Luiz Neto <luizneto.dev@outlook.com>
2025-04-09 15:20:03 -0300

    Implementação da lógica de entrada e saída de dados, além da implementação da lógica de movimento das peças

--- chess.c
+++ chess.c
@@ -2,4 +2,60 @@
 
 int main()
 {
+    int i = 1, menuChoice, square;
+
+    printf("\n\n ******* Bem-Vindo ao Chess CLI Game ******* \n");
+
+    do
+    {
+        printf("Selecione a peça que deseja mover:\n");
+        printf("1. Torre\n");
+        printf("2. Bispo\n");
+        printf("3. Rainha\n");
+        printf("4. Sair do jogo\n\n");
+        scanf("%d", &menuChoice);
+    } while (menuChoice > 4);
+
+    switch (menuChoice)
+    {
+    case 1:
+        printf("Quantas casas deseja mover a torre?\n");
+        scanf("%d", &square);
+
+        for (int i = 1; i <= square; i++)
+        {
+            printf("Direita\n");
+        }
+        break;
+
+    case 2:
+        printf("Quantas casas deseja mover o bispo?\n");
+        scanf("%d", &square);
+
+        do
+        {
+            printf("Direita\n");
+            i++;
+        } while (i <= square);
+
+        break;
+
+    case 3:
+        printf("Quantas casas deseja mover a rainha?\n");
+        scanf("%d", &square);
+
+        while (i <= square)
+        {
+            printf("Esquerda\n");
+            i++;
+        }
+
+        break;
+    case 4:
+        printf("Saindo do jogo...\n");
+        break;
+
+    default:
+        break;
+    }
 }
