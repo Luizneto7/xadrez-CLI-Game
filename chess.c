dfba1016462e1c089ea09de8111781d7f312539c
Luiz Neto <luizneto.dev@outlook.com>
2025-04-24 13:48:53 -0300

    Ajustes

--- chess.c
+++ chess.c
@@ -1,9 +1,41 @@
 #include <stdio.h>
 
+void chessPieceMovement(int square, char direction[8])
+{
+    if (square > 0)
+    {
+        printf("%s\n", direction);
+        chessPieceMovement(square - 1, direction);
+    }
+}
+
+void bishopMovement(int square)
+{
+    for (int i = square; i > 0; i--)
+    {
+        printf("Cima\n");
+
+        for (int j = 0; j < 1; j++)
+        {
+            printf("Direita\n");
+        }
+    }
+}
+void horseMovement(int movesLeft, char firstMove[10], char secondMove[10])
+{
+    if (movesLeft == 0)
+        return;
+    printf("\nMovimento 1: %s\n", firstMove);
+    printf("\nMovimento 2: %s\n", firstMove);
+    printf("\nMovimento 3: %s\n", secondMove);
+
+    horseMovement(movesLeft - 1, firstMove, secondMove);
+}
+
 int main()
 {
     int i = 1, menuChoice, square;
-    char firstHorseMove[20], lastHorseMove[20];
+    char firstMove[20], secondMove[20], direction[8];
 
     printf("\n\n ******* Bem-Vindo ao Chess CLI Game ******* \n");
 
@@ -24,52 +56,47 @@ int main()
         printf("Quantas casas deseja mover a torre?\n");
         scanf("%d", &square);
 
-        for (int i = 1; i <= square; i++)
-        {
-            printf("Direita\n");
-        }
+        printf("Informe a direção da torre: (Direita, Esquerda, Cima ou Baixo) \n");
+        scanf("%s", &direction);
+
+        printf("\nMovimentação da torre:\n");
+        chessPieceMovement(square, direction);
+        printf("\nFim da movimentação da torre.\n");
         break;
 
     case 2:
         printf("Quantas casas deseja mover o bispo?\n");
         scanf("%d", &square);
 
-        do
-        {
-            printf("Direita\n");
-            i++;
-        } while (i <= square);
+        printf("\nMovimentação do bispo:\n");
+        bishopMovement(square);
+        printf("\nFim da movimentação do bispo.\n");
 
         break;
 
     case 3:
         printf("Quantas casas deseja mover a rainha?\n");
         scanf("%d", &square);
+        printf("Informe a direção da rainha: (Direita, Esquerda, Diagonal, Cima ou Baixo) \n");
+        scanf("%s", &direction);
 
-        while (i <= square)
-        {
-            printf("Esquerda\n");
-            i++;
-        }
+        printf("\nMovimentação da rainha:\n");
+        chessPieceMovement(square, direction);
+        printf("\nFim da movimentação da rainha.\n");
 
         break;
     case 4:
+        printf("Quantas casas deseja mover o cavalo?\n");
+        scanf("%d", &square);
         printf("Informe a direção dos dois primeiros movimentos do cavalo: (Direita, Esquerda, Cima ou Baixo) \n");
-        scanf("%s", &firstHorseMove);
+        scanf("%s", &firstMove);
         printf("\nAgora informe o movimento final do cavalo\n");
-        scanf("%s", &lastHorseMove);
+        scanf("%s", &secondMove);
 
-        i = 1;
+        printf("\nMovimentação do cavalo:\n");
 
-        while (i--)
-        {
-            for (int j = 0; j < 2; j++)
-            {
-                printf("%s\n", firstHorseMove);
-            }
-
-            printf("%s\n", lastHorseMove);
-        }
+        horseMovement(square, firstMove, secondMove);
+        printf("\nFim da movimentação do cavalo.\n");
 
         break;
     case 5:
@@ -78,5 +105,6 @@ int main()
 
     default:
         break;
+        return 0;
     }
 }
